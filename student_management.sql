89ff3d5e5cffb2871c3115635fcf9c5cecad46c2
Navya sri <tnavyasri@sfouresolutions.com>
2026-05-07 15:39:18 +0530

    Add files via upload

---
+++ student_management.sql
@@ -0,0 +1,131 @@
+CREATE TABLE `student table` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `admission_number` varchar(50) NOT NULL,
+  `full_name` varchar(100) NOT NULL,
+  `date_of_birth` date NOT NULL,
+  `gender` enum('Male','Female') NOT NULL,
+  `blood_group` varchar(45) DEFAULT NULL,
+  `address` text,
+  `emergency_contact` varchar(50) DEFAULT NULL,
+  `admission_date` date NOT NULL,
+  `status` enum('applicant','enrolled','active','transferred','withdrawn','graduated','alumni','suspended') NOT NULL DEFAULT 'applicant',
+  `medical_info` text,
+  `photo` varchar(300) DEFAULT NULL,
+  `id_card_details` varchar(300) DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`student_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `admission table` (
+  `admission_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `status` enum('draft','approved','rejected') NOT NULL DEFAULT 'draft',
+  `applied_date` date NOT NULL,
+  `approved_date` date DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`admission_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `guardians table` (
+  `guardian_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `phonenumber` varchar(45) NOT NULL,
+  `email` varchar(100) DEFAULT NULL,
+  `address` text,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`guardian_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `student_guardian table` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `guardian_id` int NOT NULL,
+  `relationship` varchar(45) NOT NULL,
+  `is_primary` tinyint DEFAULT '0',
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`student_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `classes table` (
+  `class_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `class_name` varchar(200) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`class_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `section table` (
+  `section_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `section_name` varchar(100) NOT NULL,
+  `capacity` int DEFAULT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`section_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `enrollments table` (
+  `enrollment_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `class_id` int NOT NULL,
+  `section_id` int NOT NULL,
+  `academic_year` varchar(45) NOT NULL,
+  `roll_number` int NOT NULL,
+  `status` enum('active','completed') DEFAULT 'active',
+  PRIMARY KEY (`enrollment_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `promotions table` (
+  `promotion_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `from_class` int NOT NULL,
+  `to_class` int NOT NULL,
+  `academic_year` varchar(45) NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`promotion_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `student_action table` (
+  `action_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `action_type` enum('transfer','withdrawal') NOT NULL,
+  `reason` text,
+  `action_date` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`action_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `status history table` (
+  `student_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `old_status` varchar(45) DEFAULT NULL,
+  `new_status` varchar(45) DEFAULT NULL,
+  `changed_on` date NOT NULL,
+  `created_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`student_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `student documents table` (
+  `doc_id` int NOT NULL AUTO_INCREMENT,
+  `school_id` int NOT NULL,
+  `student_id` int NOT NULL,
+  `document_name` varchar(100) NOT NULL,
+  `file_path` varchar(300) NOT NULL,
+  `uploaded_at` timestamp NULL DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (`doc_id`,`school_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+
+
+
+
+
+
+
+
+
